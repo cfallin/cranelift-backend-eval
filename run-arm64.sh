5dd571c338da5f109a002d1687d543f4f6c01d08
Chris Fallin <cfallin@mozilla.com>
2020-04-14 22:43:46 -0700

    arm64 run script: five data points per config; suppress stdout/stderr of tests.

--- run-arm64.sh
+++ run-arm64.sh
@@ -13,7 +13,7 @@ fi
 run_time() {
     tmpfile=`mktemp`
     echo "Running: $@" >&2
-    /usr/bin/time -f "%E" -o $tmpfile "$@"
+    /usr/bin/time -f "%E" -o $tmpfile "$@" >/dev/null 2>&1
     if [ $? -ne 0 ]; then
         echo "FAIL"
     else
@@ -24,27 +24,27 @@ run_time() {
     fi
 }
 
-run_time_10() {
+run_time_5() {
     times=""
-    for i in `seq 0 9`; do
+    for i in `seq 0 4`; do
         t=`run_time "$@"`
         times="$times$t,"
     done
     echo $times
 }
 
-ten_commas=",,,,,,,,,,"
-echo "Bench,ARM64 Baseline Compile${ten_commas}ARM64 Baseline Comp+Run${ten_commas}ARM64 Cranelift Compile${ten_commas}ARM64 Cranelift Comp+Run${ten_commas}" > $OUT
+five_commas=",,,,,"
+echo "Bench,ARM64 Baseline Compile${five_commas}ARM64 Baseline Comp+Run${five_commas}ARM64 Cranelift Compile${five_commas}ARM64 Cranelift Comp+Run${five_commas}" > $OUT
 
 for bench in asm2wasm_*.wasm; do
     bench=`basename $bench .wasm`
     mainjs_full="${bench}.js"
     mainjs_compile="${bench}_compile_only.js"
 
-    base_compile=`run_time $VM_BASELINE $mainjs_compile`
-    base_full=`run_time $VM_BASELINE $mainjs_full`
-    cl_compile=`run_time $VM_CL $mainjs_compile`
-    cl_full=`run_time $VM_CL $mainjs_full`
+    base_compile=`run_time_5 $VM_BASELINE $mainjs_compile`
+    base_full=`run_time_5 $VM_BASELINE $mainjs_full`
+    cl_compile=`run_time_5 $VM_CL $mainjs_compile`
+    cl_full=`run_time_5 $VM_CL $mainjs_full`
 
     echo "$bench,${base_compile}${base_full}${cl_compile}${cl_full}" >> $OUT
 done
